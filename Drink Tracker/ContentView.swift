62759bb1d65900cb9c4032f60188c9cf9e868792
JP <antwwwon@icloud.com>
2022-08-09 18:57:40 -0400

    Initial Commit

---
+++ Drink Tracker/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Drink Tracker
+//
+//  Created by Joseph Antonetti on 8/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Drink Tracker/Drink_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drink_TrackerApp.swift
+//  Drink Tracker
+//
+//  Created by Joseph Antonetti on 8/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Drink_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
